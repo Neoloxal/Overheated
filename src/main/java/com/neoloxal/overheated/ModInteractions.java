6ffabc8f7cdd6f6f21d347d38ea2902b61f26331
Neoloxal <nfloeser12@gmail.com>
2026-08-20 16:22:49 -0700

    Made overheated pickaxe auto-smelt ores

--- src/main/java/com/neoloxal/overheated/ModInteractions.java
+++ src/main/java/com/neoloxal/overheated/ModInteractions.java
@@ -3,13 +3,16 @@ package com.neoloxal.overheated;
 import com.neoloxal.overheated.item.ModDataComponents;
 import com.neoloxal.overheated.item.ModItemTags;
 import com.neoloxal.overheated.item.ModItems;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LavaCauldronBlock;
@@ -18,8 +21,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.level.BlockDropsEvent;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static com.neoloxal.overheated.Overheated.overheat_map;
 import static com.neoloxal.overheated.Overheated.cooling_map;
@@ -68,6 +73,29 @@ public class ModInteractions {
                     }
 
                     event.setCanceled(true);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void blockDrops(BlockDropsEvent event) {
+        Level level = event.getLevel();
+        ItemStack stack = event.getTool();
+
+        if (stack.is(ModItems.OVERHEATED_PICKAXE.get())) {
+            for (ItemEntity drop : event.getDrops()) {
+                Optional<RecipeHolder<SmeltingRecipe>> smelted = level.getRecipeManager().getRecipeFor(RecipeType.SMELTING, new SingleRecipeInput(drop.getItem()), level);
+                Optional<RecipeHolder<BlastingRecipe>> blasted = level.getRecipeManager().getRecipeFor(RecipeType.BLASTING, new SingleRecipeInput(drop.getItem()), level);
+                if (smelted.isPresent() || blasted.isPresent()) {
+                    ItemStack newDrop;
+                    if (smelted.isPresent()) {
+                        newDrop = smelted.get().value().getResultItem(level.registryAccess());
+                    } else {
+                        newDrop = blasted.get().value().getResultItem(level.registryAccess());
+                    }
+                    newDrop.setCount(drop.getItem().getCount());
+                    drop.setItem(newDrop);
                 }
             }
         }
